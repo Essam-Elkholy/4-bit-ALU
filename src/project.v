ca24c33f5b0fa1d8dc39a3fef0f08ceb5030a99f
GlitchPi <essam2007moh@gmail.com>
2026-09-09 21:13:44 +0300

    Refactor ALU module to support 8-bit inputs

--- src/project.v
+++ src/project.v
@@ -5,143 +5,53 @@
 
 `default_nettype none
 
-module tt_um_ALU (
-    input  wire [3:0] A, B,        // 4-bit input signals
-    input  wire [3:0] ALU_FUN,     // 4-bit ALU selection signal
-    input  wire       CLK,         // Clock signal
+module tt_um_alu_4bit (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
 
-    output reg  [3:0] ALU_OUT,     // 4-bit ALU output
-    output reg        Carry_Flag,
-    output reg        Arith_Flag,
-    output reg        Logic_Flag,
-    output reg        CMP_Flag,
-    output reg        Shift_Flag
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-// Internal signal
-reg [3:0] ALU_OUT_Comb;
+wire [3:0] alu_out;
 
+wire carry_flag;
+wire arith_flag;
+wire logic_flag;
+wire cmp_flag;
+wire shift_flag;
 
-// Registered output
-always @(posedge CLK) begin
-    ALU_OUT <= ALU_OUT_Comb;
-end
+ALU alu_inst (
+    .A          (ui_in[3:0]),
+    .B          (ui_in[7:4]),
+    .ALU_FUN    (uio_in[3:0]),
+    .CLK        (clk),
 
+    .ALU_OUT    (alu_out),
+    .Carry_Flag (carry_flag),
+    .Arith_Flag (arith_flag),
+    .Logic_Flag (logic_flag),
+    .CMP_Flag   (cmp_flag),
+    .Shift_Flag (shift_flag)
+);
 
-// Combinational ALU logic
-always @(*) begin
+assign uo_out[3:0] = alu_out;
+assign uo_out[7:4] = 4'b0000;
 
-    // Default values
-    Carry_Flag  = 1'b0;
-    Arith_Flag  = 1'b0;
-    Logic_Flag  = 1'b0;
-    CMP_Flag    = 1'b0;
-    Shift_Flag  = 1'b0;
-    ALU_OUT_Comb = 4'b0000;
+assign uio_out[0] = carry_flag;
+assign uio_out[1] = arith_flag;
+assign uio_out[2] = logic_flag;
+assign uio_out[3] = cmp_flag;
+assign uio_out[4] = shift_flag;
+assign uio_out[7:5] = 3'b000;
 
-    case (ALU_FUN)
+assign uio_oe = 8'b00011111;
 
-        // Arithmetic operations
-        4'b0000: begin
-            {Carry_Flag, ALU_OUT_Comb} = A + B;
-            Arith_Flag = 1'b1;
-        end
-
-        4'b0001: begin
-            {Carry_Flag, ALU_OUT_Comb} = A - B;
-            Arith_Flag = 1'b1;
-        end
-
-        4'b0010: begin
-            ALU_OUT_Comb = A * B;
-            Arith_Flag = 1'b1;
-        end
-
-        4'b0011: begin
-            ALU_OUT_Comb = A / B;
-            Arith_Flag = 1'b1;
-        end
-
-
-        // Logic operations
-        4'b0100: begin
-            ALU_OUT_Comb = A & B;
-            Logic_Flag = 1'b1;
-        end
-
-        4'b0101: begin
-            ALU_OUT_Comb = A | B;
-            Logic_Flag = 1'b1;
-        end
-
-        4'b0110: begin
-            ALU_OUT_Comb = ~(A & B);
-            Logic_Flag = 1'b1;
-        end
-
-        4'b0111: begin
-            ALU_OUT_Comb = ~(A | B);
-            Logic_Flag = 1'b1;
-        end
-
-        4'b1000: begin
-            ALU_OUT_Comb = A ^ B;
-            Logic_Flag = 1'b1;
-        end
-
-        4'b1001: begin
-            ALU_OUT_Comb = ~(A ^ B);
-            Logic_Flag = 1'b1;
-        end
-
-
-        // Comparison operations
-        4'b1010: begin
-            CMP_Flag = 1'b1;
-
-            if (A == B)
-                ALU_OUT_Comb = 4'b0001;
-            else
-                ALU_OUT_Comb = 4'b0000;
-        end
-
-        4'b1011: begin
-            CMP_Flag = 1'b1;
-
-            if (A > B)
-                ALU_OUT_Comb = 4'b0010;
-            else
-                ALU_OUT_Comb = 4'b0000;
-        end
-
-        4'b1100: begin
-            CMP_Flag = 1'b1;
-
-            if (A < B)
-                ALU_OUT_Comb = 4'b0011;
-            else
-                ALU_OUT_Comb = 4'b0000;
-        end
-
-
-        // Shift operations
-        4'b1101: begin
-            ALU_OUT_Comb = A >> 1;
-            Shift_Flag = 1'b1;
-        end
-
-        4'b1110: begin
-            ALU_OUT_Comb = A << 1;
-            Shift_Flag = 1'b1;
-        end
-
-
-        // Default
-        default: begin
-            ALU_OUT_Comb = 4'b0000;
-        end
-
-    endcase
-end
+wire _unused = &{ena, rst_n, uio_in[7:4], 1'b0};
 
 endmodule
